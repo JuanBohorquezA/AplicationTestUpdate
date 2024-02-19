083e6cb3ac8a44090b1ab383475e867d52b09b08
JuanBohorquezA <juanbohorquezaponte@gmail.com>
2024-02-19 15:39:43 -0500

    firstCommit

---
+++ hantleDispenserV1/hantleDispenser/Domain/handlerDispenser/HandlerResponse.cs
@@ -0,0 +1,37 @@
+﻿using ControlzEx.Standard;
+using hantleDispenser.Domain;
+using HantleDispenserAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handlerDispenser.Domain
+{
+    public static class HandlerResponse
+    {
+        public static string Success = "OK";
+        public static string WithoutConnection = "No se logró conectar, valide que el numero de puerto subminitrado sea correcto e intente inicializar.";
+        public static string WithoutPhysicalConnection = "No se logró conectar, Se ha perdido la conexíón fisica con el puerto COM del dispensador.";
+        public static string CassettesDoNotMatch = "El número de cassetes agregados especificados NO SON IGUALES A LOS FISICOS.";
+        public static string SensorTimeOut = "La respuesta del Sensor a tardado más de lo esperado, valide que todo es OK e intente nuevamente.";
+        public static string RejectBoxOpen = "La caja de rechazo está mal puesta, Ingrese adecuadamente la caja de rechazo y asegurese de cerrarla correctamente con la llave.";
+        public static string CasseteDisconnected = "El baúl {0} está desconectado, Por favor comuníquese con servicio técnico de E-city.";
+        public static string CasseteDismounted = "El baúl {0} está desmontado o mal colocado, Ingrese adecuadamente el baúl";
+        public static string CasseteBadLoaded = "El baúl {0} está mal cargado, Asegurese de que no hay ningún billete mal posicionado a la salida del baúl";
+        public static string SensorCISOpen = "El compartimiento de la parte posterior del dispensador está abierto.";
+        public static string DispendTimeOut = "La respuesta del Baúl a tardado más de los esperado, intentelo nuevamente.   ";
+        public static string DispendSuccess = OperationManager.CoinsValue > 0 ? "Devuelta correcta, valor a devolver en monedas: {0}": "Devuelta correcta.";
+        public static string LastCassetteWithoutMoney = $"El último baúl se quedó sin billetes, valor a devolver en monedas {OperationManager.CoinsValue}.";
+        public static string FailMaxEject = $"No se pudo desatascar, llamar a Julio. Valor a devolver en monedas: {OperationManager.CoinsValue}.";
+        public static string StatusUndetermined = $"No se logró determinar el estado del dispensador, ocurrió un error no determinado, valor a devolver en monedas: {OperationManager.CoinsValue}";
+        public static string FailEject = "No se logró desatascar, Intentelo nuevamente";
+        public static string EjectTimeOut = "No se logró desatascar, Error la respuesta a tardado más de lo esperado, Intento nuevamente.";
+        public static string ErrorInitialize = "No se logró inicializar el dispensador: {0}";
+        public static string ErrorDispendQuantities = "No se logró dispensar: {0}";
+        public static string JAM = "Hay un atasco en el dispensador o algo está obstaculizado el paso de billetes. Revise que no halla obstaculos o polvo excesivo en el recorreido de los billetes.";
+
+    }
+
+}
---
+++ hantleDispenserV1/hantleDispenser/UserControls/ConfigUC.xaml.cs
@@ -0,0 +1,148 @@
+﻿using hantleDispenser.Domain;
+using hantleDispenser.Domain.Models;
+using hantleDispenser.Modals;
+using HantleDispenserAPI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using handlerDispenser.Domain;
+using System;
+using System.ComponentModel;
+
+namespace hantleDispenser.UserControls
+{
+    /// <summary>
+    /// Lógica de interacción para ConfigUC.xaml
+    /// </summary>
+    public partial class ConfigUC : AppUserControl
+    {
+        private ConfigViewModel _viewModel;
+        private ModalWindow? _loadModal;
+        public ConfigUC()
+        {
+            InitializeComponent();
+            _viewModel = new();
+            this.DataContext = _viewModel;
+            this.Loaded += Onloaded;
+
+        }
+        private void Onloaded(object sender, EventArgs e)
+        {
+            GetElements();
+            COMS.ItemsSource = _viewModel.COMLIST;
+            Denominations.ItemsSource = _viewModel.DENOMINATIONLIST;
+        }
+        private bool ItemSelected(ListBox listBox)
+        {
+            var item = listBox.SelectedItem;
+            if(item == null)
+                return false;
+
+            return true;
+        }
+        private void GetElements()
+        {
+            _viewModel.COMLIST = Persistence.COM_LIST;
+            _viewModel.DENOMINATIONLIST = Persistence.DENOMINATIONS_LIST;
+        }
+
+        private void BtnRefresh(object sender, MouseButtonEventArgs e)
+        {
+            GetElements();
+            COMS.ItemsSource = _viewModel.COMLIST;
+            Denominations.ItemsSource = _viewModel.DENOMINATIONLIST;
+        }
+        private void  SetDenominations()
+        {
+
+                var COMSelected = COMS.SelectedItem.ToString() ?? string.Empty;
+
+                var DenominationSelected = Denominations.SelectedItem.ToString()?.Trim().Replace("$", "").Replace(",", "").Replace(" ", "") ?? string.Empty;
+
+                SessionManager.InicializeHantleSeccion(new DenominationsQuantities { COM = COMSelected, Denomination = DenominationSelected.Split("-").ToList() });
+
+                CDMS_Handler.Portname = COMSelected;
+                CDMS_Handler.Denominations = DenominationSelected;
+
+        }
+        private  bool IsValid(List<CDMS_Response> response) 
+        {
+            if (response.Count <= 0) return false;
+            if (response[0].isSuccess) return true;
+            if (response[0].UIResponse.Contains(HandlerResponse.ErrorInitialize.Split(":")[0])) return true;
+
+            txtMessage.Text = response[0].UIResponse;
+            return false;
+        }
+
+        private async void BtnContinue(object sender, MouseButtonEventArgs e)
+        {
+            
+            txtMessage.Text = string.Empty;
+            if (!ItemSelected(COMS))
+            {
+                txtMessage.Text = "SELECCIONE UN PUERTO COM PARA CONTINUAR";
+                return;
+            }
+            if (!ItemSelected(Denominations))
+            {
+                txtMessage.Text = "SELECCIONE UNA DENOMINACIÓN PARA CONTINUAR";
+                return;
+            }
+            
+            SetDenominations();
+
+            _loadModal = _nav.ShowLoadModal("Realizando la configuración...");
+
+            var inicializeResponse = await OperationManager.GoInitialize();
+            _nav.CloseLoadModal(ref _loadModal);
+            if (!IsValid(inicializeResponse)) return;
+            
+
+            Dispatcher.Invoke(() => { Goto(new MenuUC()); });
+
+        }
+
+    }
+    public class ConfigViewModel : INotifyPropertyChanged
+    {
+        private List<string>? _COMLIST;
+        public List<string>? COMLIST
+        {
+            get
+            {
+                return _COMLIST;
+            }
+            set
+            {
+                _COMLIST = value;
+                OnPropertyRaised(nameof(COMLIST));
+            }
+        }
+
+        private List<string>? _DENOMINATIONLIST;
+        public List<string>? DENOMINATIONLIST
+        {
+            get
+            {
+                return _DENOMINATIONLIST;
+            }
+            set
+            {
+                _DENOMINATIONLIST = value;
+                OnPropertyRaised(nameof(DENOMINATIONLIST));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyRaised(string propertyname)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+
+        }
+    }
+}
